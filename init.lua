18c4ec855eeefdbf5923956ca35ada91c2b8f818
Patrick Raedler <code@readpato.dev>
2025-04-27 15:13:07 +0200

    chore: remove typescript tools, ad tsls

--- init.lua
+++ init.lua
@@ -665,13 +665,17 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        rust_analyzer = {},
-        volar = {
+        ts_ls = {
           init_options = {
-            vue = {
-              hybridMode = true,
+            plugins = {
+              {
+                name = '@vue/typescript-plugin',
+                location = require('mason-registry').get_package('vue-language-server'):get_install_path() .. '/node_modules/@vue/language-server',
+                languages = { 'vue' },
+              },
             },
           },
+          filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
         },
         -- clangd = {},
         -- gopls = {},
@@ -717,11 +721,13 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'stylua',
-        'vue-language-server',
-        'eslint_d',
         'eslint-lsp',
+        'eslint_d',
         'prettierd',
+        'rust_analyzer',
+        'stylua',
+        'ts_ls',
+        'vue-language-server',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
