fffeb304b077fc25214f07d6d820ea17a6962e38
Patrick Raedler <code@readpato.dev>
2025-04-27 16:04:11 +0200

    chore: start modularizing config

---
+++ lua/custom/plugins/catppuccin.lua
@@ -0,0 +1,18 @@
+return {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  priority = 1000,
+  opts = {
+    transparent_background = false,
+    color_overrides = {
+      mocha = {
+        base = '#000000',
+        mantle = '#000000',
+        crust = '#000000',
+      },
+    },
+    -- styles = {
+    --   italic = false,
+    -- },
+  },
+}
---
+++ lua/custom/plugins/conform.lua
@@ -0,0 +1,60 @@
+return {
+  'stevearc/conform.nvim',
+  event = { 'BufWritePre' },
+  cmd = { 'ConformInfo' },
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_format = 'fallback' }
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
+  opts = {
+    formatters = {
+      prettierd = {
+        require_cwd = true,
+      },
+    },
+    notify_on_error = false,
+    format_on_save = function(bufnr)
+      -- Disable "format_on_save lsp_fallback" for languages that don't
+      -- have a well standardized coding style. You can add additional
+      -- languages here or re-enable it for the disabled ones.
+      local disable_filetypes = { c = true, cpp = true }
+      if disable_filetypes[vim.bo[bufnr].filetype] then
+        return nil
+      else
+        return {
+          timeout_ms = 500,
+          lsp_format = 'fallback',
+        }
+      end
+    end,
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      vue = { 'eslint_d' },
+      typescriptreact = { 'prettierd' },
+      javascriptreact = { 'prettierd' },
+      typescript = function(bufnr)
+        if require('conform').get_formatter_info('prettierd', bufnr).available then
+          return { 'prettierd' }
+        else
+          return { 'eslint_d' }
+        end
+      end,
+      javascript = function(bufnr)
+        if require('conform').get_formatter_info('prettierd', bufnr).available then
+          return { 'prettierd' }
+        else
+          return { 'eslint_d' }
+        end
+      end,
+
+      -- json = { 'eslint-lsp', 'prettierd', stop_after_first = true },
+      -- jsonc = { 'eslint-lsp', 'prettierd', stop_after_first = true },
+    },
+  },
+}
