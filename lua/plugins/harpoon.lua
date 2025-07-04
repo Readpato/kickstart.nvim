f096a3d35c50c2e922be4fc89c0d5fcf49422eb2
Patrick Raedler <code@readpato.dev>
2025-07-04 14:56:38 +0200

    chore: add proper harpoon desc and config

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -11,29 +11,21 @@ return {
       settings = {
         save_on_toggle = false,
         sync_on_ui_close = false,
-        key = function()
-          return vim.loop.cwd()
-        end,
-      },
-      default = {
-        get_root_dir = function()
-          return vim.loop.cwd()
-        end,
       },
     }
 
+    for i = 1, 4 do
+      vim.keymap.set('n', '<leader>z' .. i, function()
+        harpoon:list():select(i)
+      end, { desc = '[Z]oomie: Go to harpoon ' .. i })
+    end
+
     vim.keymap.set('n', '<leader>za', function()
       harpoon:list():add()
-    end, { desc = 'Add file to harpoon' })
+    end, { desc = '[Z]oomie: [A]dd file to harpoon' })
 
     vim.keymap.set('n', '<C-e>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end, { desc = 'Toggle harpoon menu' })
-
-    for i = 1, 9 do
-      vim.keymap.set('n', '<leader>z' .. i, function()
-        harpoon:list():select(i)
-      end, { desc = 'Go to harpoon file ' .. i })
-    end
   end,
 }
